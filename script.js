9dfa72f592e83e44b9a81df7d395de1412518fd0
kuyavinzofficial <kuyavinzofficial@gmail.com>
2026-08-05 23:49:13 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
+function startGame(){
 
+    alert("Puzzle Game Loading...");
+
+}
+
+
+function openLevels(){
+
+    window.location="levels.html";
+
+}
+
+
+
+function backHome(){
+
+    window.location="index.html";
+
+}
+
+
+
+function playLevel(level){
+
+    alert("Starting PuzzleMania Level " + level);
+
+}
