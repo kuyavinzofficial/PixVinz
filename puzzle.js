65d4b54162ecebf8da571ea243879d349da73ac0
kuyavinzofficial <kuyavinzofficial@gmail.com>
2026-08-08 17:15:59 +0800

    Update puzzle.js

--- puzzle.js
+++ puzzle.js
@@ -1749,3 +1749,23 @@ function(e){
 
 
 });
+
+
+
+/* ======================================
+   PUZZLEMANIA SPLASH SCREEN
+   ====================================== */
+
+window.addEventListener("load", function(){
+
+    const splash = document.getElementById("puzzleSplash");
+
+    if(!splash) return;
+
+    setTimeout(function(){
+
+        splash.classList.add("splashHidden");
+
+    }, 3000);
+
+});
